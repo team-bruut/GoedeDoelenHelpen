f784d7462d1b46aab4e114b41ce2bc741b76befb
Barld Boot <bakeds@gmail.com>
2018-03-09 17:58:44 +0100

    Add then operator

---
+++ Extensions.Functional/ThenExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions.Functional
+{
+    public static class ThenExtensions
+    {
+        public static b Then<a, b>(this a v, Func<a, b> f) => f(v);
+        public static void Then<a>(this a v, Action<a> f) => f(v);
+    }
+}
