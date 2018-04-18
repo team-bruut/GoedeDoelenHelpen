a2e840071a7fa0e1b64a6660d7535fdcd28e2e8f
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2018-04-18 10:31:31 +0200

    Added Angular Material

---
+++ GoedeDoelenHelpen/ClientApp/src/app/app.material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatMenu, MatMenuItem, MatButton } from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatToolbarModule
+    // MatMenu,
+    // MatMenuItem,
+    // MatButton
+  ],
+  exports: [
+    MatToolbarModule
+    // MatMenu,
+    // MatMenuItem,
+    // MatButton
+  ]
+})
+export class AppMaterialModule { }
--- GoedeDoelenHelpen/ClientApp/src/app/app.module.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AppMaterialModule } from './app.material.module';
 
 @NgModule({
   declarations: [
@@ -22,11 +24,13 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    FlexLayoutModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
