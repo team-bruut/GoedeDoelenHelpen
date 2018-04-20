10a9cc871cc77b3ba18660806a9efd1f12dffd07
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2018-04-20 15:21:18 +0200

    Clarified usage image to icon

--- GoedeDoelenHelpen/ClientApp/src/app/app.component.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/app.component.ts
@@ -12,9 +12,9 @@ export class AppComponent {
   title = 'app';
 
   // icons
-  imagefolder = './assets/images/';
+  iconfolder = './assets/images/';
   constructor(iconReg: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconReg.addSvgIcon('logo', sanitizer.bypassSecurityTrustResourceUrl(this.imagefolder + 'LOGO_NO_TEXT.svg'))
-           .addSvgIcon('logo_text', sanitizer.bypassSecurityTrustResourceUrl(this.imagefolder + 'LOGO_TEXT.svg'));
+    iconReg.addSvgIcon('logo', sanitizer.bypassSecurityTrustResourceUrl(this.iconfolder + 'LOGO_NO_TEXT.svg'))
+           .addSvgIcon('logo_text', sanitizer.bypassSecurityTrustResourceUrl(this.iconfolder + 'LOGO_TEXT.svg'));
   }
 }
