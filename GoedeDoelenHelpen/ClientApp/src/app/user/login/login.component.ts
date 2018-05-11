fb4266ea36d0cbf34459e5cbc93c4fc7d4576db5
Barld Boot <bakeds@gmail.com>
2018-05-11 10:41:19 +0200

    change login to scss

--- GoedeDoelenHelpen/ClientApp/src/app/user/login/login.component.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/user/login/login.component.ts
@@ -5,7 +5,7 @@ import { AuthenticationService } from '../../authentication.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
