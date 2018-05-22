39c38b76a77a973c205825cd049fbf088b7ace8e
fjwillemsen <fjwillemsen@icloud.com>
2018-05-22 16:46:08 +0200

    Created a dynamic and responsive progress bar from scratch including steps, added user profile picture replacement, improved dashboard and theme colors, improved logo consistency

--- GoedeDoelenHelpen/ClientApp/src/app/user/dashboard/progress-bar/progress-bar.component.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/user/dashboard/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, ViewChild, ElementRef } from '@angular/core';
 import { ChartSettings } from './../dashboard.component';
 
 @Component({
@@ -7,24 +7,61 @@ import { ChartSettings } from './../dashboard.component';
   styleUrls: ['./progress-bar.component.scss']
 })
 export class ProgressBarComponent implements OnInit {
-  cs = new ChartSettings();
+  @ViewChild('progressBar') progressBar: ElementRef;
 
-  chartType = 'linear-gauge';
-  view = [300, 100];
-  scheme = this.cs.getColorScheme(this.cs.colorScheme);
-  animations = true;
-  gaugeMin = 0;
-  gaugeMax = 5;
-  gaugeLargeSegments = 5;
-  gaugeTextValue = 'Progress';
-  gaugeUnits = 'Niveau';
-  gaugeShowAxis = true;
-  gaugeValue = 2; // linear gauge value
-  gaugePreviousValue = 3;
+  level = 1;
+  levelString: String;
+  // levelArray = ['Beginner', 'Enthousiasteling', 'Professional', 'Expert', 'Legend'];
+  levelArray = [
+    { 'name': 'Beginner', 'steps': [] },
+    { 'name': 'Enthousiasteling', 'steps': [
+        { 'desc': 'Je Facebook account toe te voegen', 'link': '<>', 'done': true },
+        { 'desc': 'Vandaag iets te posten', 'link': '<>', 'done': false },
+        { 'desc': 'Je Twitter account toe te voegen', 'link': '<>', 'done': false }
+      ] },
+    { 'name': 'Professional', 'steps': [
+      { 'desc': 'Je LinkedIn account toe te voegen', 'link': '<>', 'done': false },
+      { 'desc': 'Deze week twee keer  iets te posten', 'link': '<>', 'done': true },
+      { 'desc': 'Je Hyves account toe te voegen', 'link': '<>', 'done': true }
+    ] },
+    { 'name': 'Expert', 'steps': [] },
+    { 'name': 'Legend', 'steps': [] }
+  ];
+  maxLevels = this.levelArray.length;
 
-  constructor() { }
+  value = 0;
+  v: number;
+  valueString: string;
+  maxValue = 100;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.updateLevel();
+    this.updateValue();
+  }
+
+  updateLevel() {
+    if (this.level > this.maxLevels) {
+      this.level = this.maxLevels;
+    }
+    this.levelString = this.levelArray[this.level - 1].name;
+  }
+
+  updateValue() {
+    this.value = (this.level - 1) * 25;
+    if (this.value > this.maxValue) {
+      this.value = this.maxValue;
+    }
+
+    this.v = 2.3 - ((this.level - 1) * 0.6); // adds some space to hit the full points
+    this.valueString = this.value + this.v + '%';
+  }
+
+  move(addLevels: number) {
+    this.level += addLevels;
+    this.updateLevel();
+    this.updateValue();
+  }
 }
