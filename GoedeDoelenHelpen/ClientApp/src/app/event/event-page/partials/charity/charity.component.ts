4d7bae0933047f16df0a69c594441fb1d3576408
fjwillemsen <fjwillemsen@icloud.com>
2018-06-09 11:40:47 +0200

    Made Comments and Charity component, improved overall layout

--- GoedeDoelenHelpen/ClientApp/src/app/event/event-page/partials/charity/charity.component.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/event/event-page/partials/charity/charity.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharityComponent implements OnInit {
 
+  charity: {
+    name: string,
+    description: string,
+  };
+
   constructor() { }
 
   ngOnInit() {
+    this.charity = {
+      name: 'WEM Nederland',
+      description: 'Het is de missie van de christelijke stichting WEM-Nederland en haar partners om weeskinderen in Muanda van DR Congo de beste leefomstandigheden te bieden, waarbij ieder weeskind beschikt over een waardevol thuis. Dit doet WEM-Nederland door middel van het financieren en meebouwen aan familiehuizen, waterputten, boerderijen, een bakkerij en een school. Ook ondersteunt WEM- Nederland de mogelijkheid om (vak)onderwijs te volgen, zodat ieder kind een beroep kan gaan uitoefenen en uitzicht heeft op een mooie toekomst.',
+    };
   }
 
 }
--- GoedeDoelenHelpen/ClientApp/src/app/event/event-page/partials/comments/comments.component.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/event/event-page/partials/comments/comments.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit {
 
+  comments: {
+    message: string,
+    user: string,
+    amount: string
+  }[];
+
   constructor() { }
 
   ngOnInit() {
+    this.comments = [
+      {
+        message: 'Gaaf dat je dit organiseert!',
+        user: 'Tommie',
+        amount: '5,00',
+      },
+      {
+        message: 'Veel succes! Dat moet helemaal goed gaan komen met jouw instelling!',
+        user: 'K. van Dam',
+        amount: '23,00',
+      },
+      {
+        message: 'Hopen dat hier heel veel kinderen mee geholpen zijn',
+        user: 'Mama van Koen',
+        amount: '15,00',
+      }
+    ];
   }
 
 }
