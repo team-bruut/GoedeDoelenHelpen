75c44ac417b0534c82b5896e2da36546b446dec0
fjwillemsen <fjwillemsen@icloud.com>
2018-06-14 16:36:46 +0200

    Made the 404 page in accordance with the design, made the mobile menu reflect the logged in state

--- GoedeDoelenHelpen/ClientApp/src/app/page-not-found/page-not-found.component.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/page-not-found/page-not-found.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavMenuService } from '../nav-menu/nav-menu.service';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent implements OnInit {
+export class PageNotFoundComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private navMenuService: NavMenuService, private router: Router) {
+    this.navMenuService.setTheme('registration');
+  }
 
   ngOnInit() {
   }
 
+  toHome() {
+    this.router.navigate(['/']);
+  }
+
+  ngOnDestroy() {
+    this.navMenuService.setTheme('default');
+  }
 }
