d5f749dcced2ebf47eda9910417d6ef52aa3dd09
fjwillemsen <fjwillemsen@icloud.com>
2018-06-19 15:36:30 +0200

    Dashboard: Made comments section, improved donated amount to chart, added a goals chart, connected buttons, improved default for page selector

--- GoedeDoelenHelpen/ClientApp/src/app/user/dashboard/donations-chart/donations-chart.component.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/user/dashboard/donations-chart/donations-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartSettings } from '../dashboard.component';
 
 @Component({
   selector: 'app-donations-chart',
@@ -6,10 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./donations-chart.component.scss']
 })
 export class DonationsChartComponent implements OnInit {
+  cs = new ChartSettings();
+
+  legendTitle: string;
+  xScaleMin: any;
+  xScaleMax: any;
+  yScaleMin: number;
+  yScaleMax: number;
+  timeline: boolean;
+  label: string;
+
+  chartType: string;
+  view: number[];
+  results: any[];
+
+  scheme: object;
+  schemeType: string;
+
+  animations: boolean;
+  tooltipDisabled: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.animations = true;
+    this.tooltipDisabled = false;
+    this.legendTitle = '';
+    this.chartType = 'pie-grid';
+    this.label = 'Totaal';
+
+    this.scheme = this.cs.getColorScheme(this.cs.colorScheme);
+    this.schemeType = 'ordinal';
+
+    this.results = [
+      {
+        'name': '1000 bezoekers',
+        'value': 542
+      },
+      {
+        'name': 'Donaties',
+        'value': 279
+      },
+    ];
   }
 
+  pieTooltipText({data}) {
+    const label = data.name.toLocaleString();
+    const val = data.value.toLocaleString();
+
+    return `
+      <span class="tooltip-label">${label}</span>
+      <span class="tooltip-val">${val}</span>
+    `;
+  }
 }
--- GoedeDoelenHelpen/ClientApp/src/app/user/dashboard/live-comments/live-comments.component.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/user/dashboard/live-comments/live-comments.component.ts
@@ -7,12 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveCommentsComponent implements OnInit {
 
-  liveComments: string[];
+  comments: {
+    message: string,
+    user: string,
+    amount: string
+  }[];
 
   constructor() { }
 
   ngOnInit() {
-    this.liveComments = ['This platform is awesome!', 'Check out this website guys!', 'Donate for Save The Children now'];
+    this.comments = [
+      {
+        message: 'Gaaf dat je dit organiseert!',
+        user: 'Tommie',
+        amount: '5,00',
+      },
+      {
+        message: 'Veel succes! Dat moet helemaal goed gaan komen met jouw instelling!',
+        user: 'K. van Dam',
+        amount: '23,00',
+      },
+      {
+        message: 'Hopen dat hier heel veel kinderen mee geholpen zijn',
+        user: 'Mama van Koen',
+        amount: '15,00',
+      }
+    ];
   }
 
 }
