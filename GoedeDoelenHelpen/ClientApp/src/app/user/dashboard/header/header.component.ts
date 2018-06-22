8d2c3b8fb29acf163a955a22555292d6ef533d16
Barld Boot <bakeds@gmail.com>
2018-06-22 07:55:31 +0200

    display firstname on dashboard

--- GoedeDoelenHelpen/ClientApp/src/app/user/dashboard/header/header.component.ts
+++ GoedeDoelenHelpen/ClientApp/src/app/user/dashboard/header/header.component.ts
@@ -3,6 +3,8 @@ import { FbService } from '../../../facebook/fb.service';
 import { FBAuthModel } from '../../../facebook/FBAuthModel';
 import { AuthenticationService } from '../../../authentication.service';
 import { WindowWrapper } from '../../../classes/windowwrapper/windowwrapper';
+import { Observable } from '../../../../../node_modules/rxjs';
+import { map } from '../../../../../node_modules/rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -13,23 +15,25 @@ export class HeaderComponent implements OnInit {
 
   images = '../../../assets/images/dashboard';
 
-  pages = [{ title: "WNF", id: "GPXQD" }, { title: "Power2Fly", id: "JDPQX" }, { title: "Nierstichting", id: "OBOKO" }];
+  pages = [{ title: 'WNF', id: 'GPXQD' }, { title: 'Power2Fly', id: 'JDPQX' }, { title: 'Nierstichting', id: 'OBOKO' }];
 
   constructor(private authService: AuthenticationService,
     private windowWrapper: WindowWrapper,
-    private fbService : FbService) {
-    
+    private authenticationService: AuthenticationService,
+    private fbService: FbService) {
+
+  }
+
+  get username(): Observable<string> {
+    return this.authService.AuthenticationInfo.pipe(map(info => info.loggedIn ? info.firstName : ''));
   }
 
   ngOnInit() {
 
   }
 
-  ngAfterContentInit() {
-  }
-
-  fbAuth(): boolean { //Checks if user is authorized with facebook through backend
-    if (this.fbService != undefined) {
+  fbAuth(): boolean { // Checks if user is authorized with facebook through backend
+    if (this.fbService !== undefined) {
       return (this.fbService.loggedIn());
     } else {
       return true;
